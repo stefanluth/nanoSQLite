21e56571daed93b1fd5913089c279c2a16034397
stefanluth <stefan.luthardt@outlook.com>
2023-08-06 02:11:38 +0200

    add repository url, license, author

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="nanoSQLite",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/stefanluth/nanoSQLite",
+    license="MIT",
+    author="Stefan Luthardt",
 )
