d0dd98707e2be60267e7000047ad2f1c175b4bf3
stefanluth <stefan.luthardt@outlook.com>
2023-08-14 21:18:35 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="nanoSQLite",
-    version="1.0.1",
+    version="1.1.0",
     packages=find_packages("./nanoSQLite"),
     description="A lightweight wrapper for the SQLite3 Python module.",
     long_description=open("README.md").read(),
