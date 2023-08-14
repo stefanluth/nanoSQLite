925e0375dc501da12f162cfe0dbbae0f8e6dfd55
stefanluth <stefan.luthardt@outlook.com>
2023-08-14 21:16:00 +0200

    refactor type checking

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from nanoSQLite import NanoSQLite
+from nanoSQLite import NanoSQLite, Types
 
 
 @pytest.fixture()
@@ -19,10 +19,10 @@ def test_create_empty_table(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
@@ -43,10 +43,10 @@ def test_insert_data(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
@@ -68,10 +68,10 @@ def test_select_data(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
@@ -90,10 +90,10 @@ def test_update_data(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
@@ -118,10 +118,10 @@ def test_delete_data(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
@@ -143,13 +143,61 @@ def test_drop_table(db: NanoSQLite):
         "test_table",
         {
             "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "name": "TEXT",
-            "age": "INTEGER",
-            "height": "REAL",
-            "is_cool": "BOOLEAN",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
         },
     )
 
     db.delete_table("test_table")
 
     assert db.tables == {}
+
+
+def test_wrong_type_data_insertion(db: NanoSQLite):
+    db.create_table(
+        "test_table",
+        {
+            "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
+        },
+    )
+
+    with pytest.raises(TypeError):
+        db.insert("test_table", {"name": "John", "age": 20, "height": 1.8, "is_cool": "bool"})
+
+    with pytest.raises(TypeError):
+        db.insert("test_table", {"name": "John", "age": 20, "height": "eighteen.three", "is_cool": True})
+
+    with pytest.raises(TypeError):
+        db.insert("test_table", {"name": "John", "age": "twelve", "height": 1, "is_cool": True})
+
+
+def test_attempt_sql_injection(db: NanoSQLite):
+    db.create_table(
+        "test_table",
+        {
+            "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
+            "name": Types.TEXT,
+            "age": Types.INTEGER,
+            "height": Types.REAL,
+            "is_cool": Types.BOOLEAN,
+        },
+    )
+
+    with pytest.raises(TypeError):
+        db.insert("test_table", {"name": "John", "age": 20, "height": 1.8, "is_cool": True, "is_cool": "1 OR 1=1"})
+
+    db.insert(
+        "test_table",
+        {"name": "John; DROP TABLE test_table", "age": 20, "height": 1.8, "is_cool": True},
+    )
+
+    assert db.select_first("test_table", ["name", "age"], {"age": 20}) == {
+        "name": "John; DROP TABLE test_table",
+        "age": 20,
+    }
